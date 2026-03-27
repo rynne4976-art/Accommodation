fd6007f84a5089b49c29c3a18f90fc6d495dd402
rynne4976-art <rynne4976@gmail.com>
2026-03-27 12:52:00 +0900

    member 기능 구현 회원가입, DB연동 SecurityConfig 추가

--- src/main/java/com/Accommodation/AccommodationApplication.java
+++ src/main/java/com/Accommodation/AccommodationApplication.java
@@ -2,8 +2,10 @@ package com.Accommodation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AccommodationApplication {
 
 	public static void main(String[] args) {
